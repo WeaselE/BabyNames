2988a5e85e9e202f504a2eb6a119c6b3e522ec4e
weaustin <weaustin@iu.edu>
2021-04-14 10:40:39 -0400

    initial file setup.

--- graphpopularity.py
+++ graphpopularity.py
@@ -2,4 +2,15 @@ import pandas as pd
 import numpy as np
 
 def loading():
+    fl = open("baby-names-by-state.csv", "r")
+    df = pd.read_csv(fl)
+    return df
+
+def filter_name():
     pass
+
+def create_graph():
+    pass
+
+if __name__=="main":
+    df = loading()
