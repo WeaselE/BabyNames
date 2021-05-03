1604dc60c4fb3f684d9ab5ea66c284503c1b107f
weaustin <weaustin@iu.edu>
2021-05-03 16:43:17 -0400

    Completed project, made graphs for random names

--- graphpopularity.py
+++ graphpopularity.py
@@ -1,3 +1,6 @@
+#Data Source: US Social Security Bureau
+
+
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -10,17 +13,15 @@ fl.close()
 
 def filter_name(df, name):
     df = df[df['name'] == name]
-    print(df)
     df = df.groupby("year")
-    print(df)
     df = df["number"].sum().reset_index()
-    print(df)
     # df_grouped.reset_index()
     # new_df = df_grouped.nsmallest(len(new_df), "year")
     # print(new_df["year"])
     return df
 
 def create_graph(graphplt):
+    #Plotting to display to user
     plt.plot(graphplt["year"], graphplt["number"], label=nm)
     plt.xlabel("Year")
     plt.ylabel("Count of name that year")
@@ -30,15 +31,14 @@ def create_graph(graphplt):
     mx = graphplt.max()
     mxy = int(mx[1])
     divisions = int(mxy / 10)
-    print(divisions)
     plt.yticks([i for i in range(mxy, 0, -divisions)])
+    plt.savefig("Naming count of " + nm + ".png", dpi=1000)
     plt.show()
-    # plt.savefig("count of " + nm + ".png", dpi=1000)
+    
 
 if __name__ == "__main__":
     nm = input("Please enter a name here: ")
     nm = nm.strip()
-    print(nm)
     if nm == "":
         nm = "David"
     filtered = filter_name(df, nm)
